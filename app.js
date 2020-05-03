a15c00c52dc0b1cfb5dfd72645f8e18054c92347
Administrateur <administrateur@MacBook-Air-de-Administrateur.local>
2020-05-03 10:15:47 +0200

    adding dynamic realtions and passing from then catch to async functions Node and mongoDB

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-// state of advancement = 6
-// goal = adding relations between different tables
-
+// state of advancement = 7
+// goal = create dynamic relations 
 
 // node framework
 const express = require('express');
@@ -8,131 +7,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 // mongoose library
 const mongoose = require('mongoose');
-// bcrypt library 
-const bcrypt = require('bcryptjs');
+
 // package that exports a function that helps funneling requests through the grapql query parser
 const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
 const app = express();
 
-// retrieve Schema
-const Event = require('./models/Event');
-const User = require('./models/User');
+// import schema 
+const graphQlSchema = require('./graphql/schema/index');
+const graphQlResolvers = require('./graphql/resolvers/index');
 
 app.use(bodyParser.json());
 
-app.use('/graphql', graphqlHttp({
-    schema: buildSchema(`
-        type Event {
-            _id: ID!
-            title: String!
-            description: String! 
-            price: Float!
-            date : String!
-        }
-        type User {
-            _id: ID!
-            email: String!
-            password: String
-            
-        }
 
-        input eventInput{
-            title: String!
-            description: String!
-            price: Float!
-            date: String!    
-        } 
-
-        input userInput{
-            email: String!
-            password: String!
-        }
-
-        type RootQuery {
-            events: [Event!]!
-        }
-        type RootMutation {
-            createEvent(eventInput: eventInput): Event
-            createUser(userInput: userInput): User
-        }
-        schema {
-            query : RootQuery
-            mutation : RootMutation
-        }
-    `),
-    rootValue: {
-        events: () => {
-            return Event.find()
-                .then(events => {
-                    return events.map(event => {
-                        return { ...event._doc };
-                    })
-                })
-                .catch(err => {
-                    console.log(err);
-                });
-        },
-        createEvent: (args) => {
-            const event = new Event({
-                title: args.eventInput.title,
-                description: args.eventInput.description,
-                price: +args.eventInput.price,
-                date: new Date(args.eventInput.date),
-                creator: "5ea5afb2bacb0a291e197e47"
-            });
-            let createdEvent;
-            return event.save()
-                .then(result => {
-                    createdEvent = { ...result._doc, _id: result._doc._id.toString() };
-                    return User.findById('5ea5afb2bacb0a291e197e47');
-                })
-                .then(user => {
-                    if (!user) {
-                        throw new Error('User not found.');
-                    }
-                    user.createdEvents.push(event);
-                    return user.save();
-                })
-                .then(result => {
-                    return createdEvent;
-                })
-                .catch(err => {
-                    console.log(err);
-                    throw err;
-                });
-        },
-        createUser: (args) => {
-            return User.findOne({ email: args.userInput.email })
-                .then(user => {
-                    if (user) {
-                        throw new Error('user exists already !');
-                    }
-                    return bcrypt.hash(args.userInput.password, 12)
-                })
-                .then(hashedPassword => {
-                    const user = new User({
-                        email: args.userInput.email,
-                        password: hashedPassword
-                    });
-                    return user.save()
-                        .then(result => {
-                            return { ...result._doc, password: null };
-                        })
-                        .catch(err => {
-                            throw err;
-                        })
-                })
-                .catch(err => {
-                    throw err
-                });
-
-        }
-    },
+app.use(
+  '/graphql',
+  graphqlHttp({
+    schema: graphQlSchema,
+    rootValue: graphQlResolvers,
     graphiql: true
-})
-
+  })
 );
+
 
 
 mongoose.connect(`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@eventbooking-ouibp.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`)
